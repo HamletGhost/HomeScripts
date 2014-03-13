049f40235f4df29f41f23d54851317853452b5d4
Gianluca Petrillo <petrillo@fnal.gov>
2014-03-12 19:17:05 -0500

    Additional aliases, added env.sh script to be called by bashrc

---
+++ bin/env/env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# bash script which is supposed to be sourced - no new shells are opened,
+# no exit statement is used
+#
+
+: ${EnvDir:="${1:-${HOME}/env.d}"}
+
+if [[ -d "$EnvDir" ]]; then
+	for EnvFile in ${EnvDir}/*.sh ; do
+		# skip non-executable and backup files:
+		if [[ ! -x "$EnvFile" ]] || [[ "${EnvFile: -1}" == '~' ]]; then
+			continue
+		fi
+	#	echo "Configuration: $(basename "$EnvFile")"
+	        source "$EnvFile"
+	done
+	unset EnvFile
+fi
+unset EnvDir
+
