add64602f09edd225d2c9036543aa9b343db0d9a
Gianluca Petrillo <petrillo@fnal.gov>
2014-09-18 17:53:36 -0500

    Added options to SplitPath.sh ('--help' for terse help)

--- bin/common/SplitPath.sh
+++ bin/common/SplitPath.sh
@@ -1,17 +1,72 @@
 #!/bin/bash
+#
+# Usage:  SplitPath.sh Spec [Spec ...]
+# 
+# Options can be inserted anywhere and will affect only the following arguments:
+#  -v     Spec is a variable name (default)
+#  -e     Spec is directly a value
+#  -sSEP  use SEP as separator from now on (default: ':')
+#  --     no more options
+#
 
 : ${SEP:=":"}
 
-for VarName in "$@" ; do
-	case "$VarName" in
-		( '@lib' )
-			VarName='LD_LIBRARY_PATH'
-			;;
-		( '@bin' )
-			VarName='PATH'
-			;;
-	esac
+function help() {
+	cat <<-EOH
+	Prints the paths in the arguiments, one per line.
+	
+	Usage:  SplitPath.sh Spec [Spec ...]
+	
+	Options can be inserted anywhere and will affect only the following arguments:
+	 -v --varname Spec is a variable name (default)
+	 -e --value   Spec is directly a value
+	 -sSEP        use SEP as separator from now on (default: ':')
+	 -h -? --help print this help message
+	 --           no more options
+	EOH
+} # help()
+
+
+declare -i NoMoreOptions=0
+declare Mode="var"
+
+for Arg in "$@" ; do
+	# check if it's a mode option
+	if [[ "${Arg:0:1}" == '-' ]] && [[ $NoMoreOptions == 0 ]]; then
+		case "$Arg" in
+			( '-v' | '--var' | '--varname' )
+				Mode="var"
+				continue
+				;;
+			( '-e' | '--value' )
+				Mode="val"
+				continue
+				;;
+			( "-s"* )
+				SEP="${Arg#-s}"
+				continue
+				;;
+			( "-h" | '-?' | '--help' )
+				help
+				continue
+				;;
+			( "--" )
+				NoMoreOptions=1
+				continue
+				;;
+		esac
+	fi
+	
+	if [[ "$Mode" == 'var' ]]; then
+		case "$Arg" in
+			( '@lib' ) VarName='LD_LIBRARY_PATH' ;;
+			( '@bin' ) VarName='PATH' ;;
+		esac
+		Value="${!VarName}"
+	else
+		Value="$Arg"
+	fi
 #	echo "${VarName}"
-	tr "$SEP" "\n" <<< "${!VarName}"
+	tr "$SEP" "\n" <<< "$Value"
 done
 
