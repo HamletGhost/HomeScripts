be3c46e1eefb01d6d8f97800f93b20e8567bd061
Hamlet <_hamlet@libero.it>
2015-12-27 18:39:01 -0600

    Merging OSX and GNU

--- env.d/10_aliases.sh
+++ env.d/10_aliases.sh
@@ -1,11 +1,19 @@
 # requires the ??-functions to be loaded
 [[ -n "$FUNCTIONS_SH_LOADED" ]] || echo "Warning: bash functions not loaded. Some functions will be missing." >&2
 
+# this is fundamental enough that we provide a local implementation here:
+declare -F isBSD > /dev/null || function isBSD() { [[ "$(uname)" != "Linux" ]]; }
+
 # common aliases
-alias du0="du -x --max-depth=0"
-alias du1="du -x --max-depth=1"
-alias du2="du -x --max-depth=2"
-alias v="ls --color=auto -alv"
+alias du0="du -x -d0"
+alias du1="du -x -d1"
+alias du2="du -x -d2"
+
+if isBSD ; then
+  alias v="ls -Gal"
+else
+  alias v="ls --color=auto -alv"
+fi
 alias rgrep="grep -R"
 alias vi='vim'
 
@@ -19,11 +27,6 @@ alias whenami="date"
 alias whyami="echo 42"
 
 function go() { local Dir="$1" ; cd "$Dir" && ls ; }
-
-function chdirl() {
-	local Dir="$1"
-	chdir "$Dir" && mkdir "logs"
-}
 
 # function rl() { readlink -f "${1:-.}" ; }
 # full_path() is defined in functions.sh
