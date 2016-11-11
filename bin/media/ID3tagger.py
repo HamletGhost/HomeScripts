b42cec2fb9c39d605dde44a9926c06db5e1f8a20
Hamlet <_hamlet@libero.it>
2016-11-11 10:00:00 -0600

    Sorting tracks from a playlist made easier.

--- bin/media/ID3tagger.py
+++ bin/media/ID3tagger.py
@@ -5,7 +5,32 @@
 # Very primitive error handling here. Sorry.
 #
 
+__doc__ = """"Creates a script assigning ID3 tags to MP3 files.
+
+The input file is interpreted as a sequence of "key: value" pairs.
+Lines starting with a "#" are interpreted as comments.
+The valid keys are:
+- file (mandatory): the file to change
+- album, artist (also: author), genre, year (also: date), title: standard ID3
+    keys
+- track (also: trackno): standard ID3 key, with value in the form of a single
+    number representing the track number, or two numbers "#/#", where the
+    second one represents the total number of tracks
+- any other key is added in the "comments" ID3 tag
+
+Track number and total number are always assigned. If they are not specified,
+they are computed automatically from the position in the list and the total
+number of entries in the list, respectively.
+
+Finally, if a name is not in the form "key: value", it is considered to be the
+file name (that is, the "file:" key is to be considered optional).
+
+The file path element must be the first one of the block of metadata pertaining
+that file.
+"""
+
 import sys
+import os.path
 import logging
 import copy
 import re
@@ -177,215 +202,235 @@ class ID3GenresClass:
 
 
 class BashQuoterClass:
-	
-	NonSimpleWord = re.compile('[^a-zA-Z0-9._-]')
-	
-	@staticmethod
-	def isSimpleWord(s): return BashQuoterClass.NonSimpleWord.search(s) is None
-		
-	@staticmethod
-	def Quote(s):
-		# might do it nicer...
-		if BashQuoterClass.isSimpleWord(s): return s;
-		return "'%s'" % s.replace("'", "'\\''")
-	# Quote()
-	
-	@staticmethod
-	def QuoteList(l):
-		return [ BashQuoterClass.Quote(s) for s in l ]
-	# QuoteList()
+   
+   NonSimpleWord = re.compile('[^a-zA-Z0-9._-]')
+   
+   @staticmethod
+   def isSimpleWord(s): return BashQuoterClass.NonSimpleWord.search(s) is None
+      
+   @staticmethod
+   def Quote(s):
+      # might do it nicer...
+      if BashQuoterClass.isSimpleWord(s): return s;
+      return "'%s'" % s.replace("'", "'\\''")
+   # Quote()
+   
+   @staticmethod
+   def QuoteList(l):
+      return [ BashQuoterClass.Quote(s) for s in l ]
+   # QuoteList()
 
 # class BashQuoterClass
 
 
 class ProcessorClass:
-	def __init__(self):
-		self.state = { }
-		self.output = []
-		self.commentKeys = set()
-	# __init__()
-	
-	def Parse(self, InputSpecs):
-		
-		for iLine, spec in enumerate(InputSpecs):
-			spec = spec.strip();
-			
-			# empty line
-			if not spec: continue
-			
-			# support full line comments
-			if spec.startswith('#'): continue
-			
-			key, value = map(str.strip, spec.split(':', 1))
-			key = key.lower()
-			
-			if key == "file":
-				if 'InputFile' in self.state: self.OutputCurrent()
-				self.state['InputFile'] = value
-				continue
-			# if file
-			
-			for keyword, labels in (
-			  ( 'Album', 'album' ),
-			  ( 'Artist', ( 'artist', 'author', ) ),
-			  ( 'Genre', 'genre' ),
-			  ( 'Year', ( 'year', 'date', ) ),
-			  ( 'Title', 'title' ),
-			  ( 'TrackNo', ( 'track', 'trackno' ) ),
-			  ( 'NTracks', ( 'ntracks', 'tracks' ) ),
-			  ):
-				
-				if isinstance(labels, basestring): labels = ( labels, )
-				for label in labels:
-					if key == label.lower(): break
-				else: continue
-				
-				self.state[keyword] = value
-				break
-			else: # a comment?
-				self.state.setdefault('Comments', []).append("%s:%s" % (key, value))
-			# if ...
-		# for
-		
-		if 'InputFile' in self.state: self.OutputCurrent()
-		
-	# Parse()
-	
-	def OutputCurrent(self):
-		self.output.append(copy.deepcopy(self.state))
-	# OutputCurrent()
-	
-	
-	def ValidateComment(self, comment):
-		key, value = comment.split(':', 1)
-		self.commentKeys.add(key.lower())
-		return key, value
-	# ValidateComment()
-	
-	
-	def OutputElement(self, item, iElem, nElem):
-		try:
-			InputFile = item['InputFile']
-		except KeyError:
-			raise RuntimeError("No file specified for item %d!", iElem)
-		
-		cmd = [ "id3v2" ]
-		
-		for key, option in (
-		  ( 'Album', '--album' ),
-		  ( 'Artist', '--artist' ),
-		  ( 'Year', '--year' ),
-		  ( 'Title', '--song' ),
-		  ):
-			try:
-				cmd.extend((option, item[key] ))
-			except KeyError: pass
-		# for
-		
-		Genre = item.get('Genre', None)
-		if Genre:
-			GenreName = ID3GenresClass.Genre(Genre)
-			if not GenreName:
-				logging.warning("Unknown genre for '%s': '%s'", InputFile, Genre)
-			cmd.extend(('--genre', GenreName, ))
-		# if
-		
-		iTrack = item.get('TrackNo', iElem + 1)
-		try:
-			iTrack, nTracks = map(int, iTrack.split('/', 1))
-		except (ValueError, AttributeError, ):
-			nTracks = item.get('NTracks', nElem)
-		cmd.extend(( '--track', "%s/%s" % (str(iTrack), str(nTracks)), ))
-		
-		for comment in item.get('Comments', []):
-			self.ValidateComment(comment)
-			cmd.append(( '--comment', comment, ))
-		# for
-		
-		cmd.append(InputFile)
-		return cmd
-		
-	# OutputCurrent()
-	
-	
-	def Output(self, OutputFile):
-		items = self.output
-		nItems = len(items)
-		Padding = len(str(nItems))
-		for iItem, item in enumerate(items):
-			
-			try:
-				InputFile = item['InputFile']
-			except KeyError:
-				raise RuntimeError("No file specified for item %d!", iItem)
-			cmd = (
-			  'echo',
-			  "[%0*d/%0*d] Processing '%s'" % (Padding, iItem+1, Padding, nItems, InputFile)
-			)
-			print >>OutputFile, " ".join(BashQuoterClass.QuoteList(cmd))
-			
-			cmd = self.OutputElement(item, iItem, nItems)
-			print >>OutputFile, " ".join(BashQuoterClass.QuoteList(cmd))
-		# for
-		cmd = [ 'echo', "Done." ]
-		
-		self.output = []
-		return nItems
-	# Output()
-	
+   def __init__(self):
+      self.state = { }
+      self.output = []
+      self.commentKeys = set()
+      self.baseDirectory = ''
+   # __init__()
+   
+   def ParseFile(self, InputSpecs):
+      
+      try:
+         fileName = InputSpecs.name
+         self.baseDirectory = os.path.dirname(fileName)
+      except AttributeError:
+         self.baseDirectory = ''
+      
+      return self.Parse(InputSpecs)
+      
+   # ParseFile
+   
+   
+   def Parse(self, InputSpecs):
+      
+      for iLine, spec in enumerate(InputSpecs):
+         spec = spec.strip();
+         
+         # empty line
+         if not spec: continue
+         
+         # support full line comments
+         if spec.startswith('#'): continue
+         
+         # if no colon is found, then we assume it's the file path
+         try:
+            key, value = map(str.strip, spec.split(':', 1))
+            key = key.lower()
+         except ValueError:
+            key = "file"
+            value = spec
+         #
+         
+         if key == "file":
+            if 'InputFile' in self.state: self.OutputCurrent()
+            self.state['InputFile'] = os.path.join(self.baseDirectory, value)
+            continue
+         # if file
+         
+         for keyword, labels in (
+           ( 'Album', 'album' ),
+           ( 'Artist', ( 'artist', 'author', ) ),
+           ( 'Genre', 'genre' ),
+           ( 'Year', ( 'year', 'date', ) ),
+           ( 'Title', 'title' ),
+           ( 'TrackNo', ( 'track', 'trackno' ) ),
+           ( 'NTracks', ( 'ntracks', 'tracks' ) ),
+           ):
+            
+            if isinstance(labels, basestring): labels = ( labels, )
+            for label in labels:
+               if key == label.lower(): break
+            else: continue
+            
+            self.state[keyword] = value
+            break
+         else: # a comment?
+            self.state.setdefault('Comments', []).append("%s:%s" % (key, value))
+         # if ...
+      # for
+      
+      if 'InputFile' in self.state: self.OutputCurrent()
+      
+   # Parse()
+   
+   def OutputCurrent(self):
+      self.output.append(copy.deepcopy(self.state))
+   # OutputCurrent()
+   
+   
+   def ValidateComment(self, comment):
+      key, value = comment.split(':', 1)
+      self.commentKeys.add(key.lower())
+      return key, value
+   # ValidateComment()
+   
+   
+   def OutputElement(self, item, iElem, nElem):
+      try:
+         InputFile = item['InputFile']
+      except KeyError:
+         raise RuntimeError("No file specified for item %d!", iElem)
+      
+      cmd = [ "id3v2" ]
+      
+      for key, option in (
+        ( 'Album', '--album' ),
+        ( 'Artist', '--artist' ),
+        ( 'Year', '--year' ),
+        ( 'Title', '--song' ),
+        ):
+         try:
+            cmd.extend((option, item[key] ))
+         except KeyError: pass
+      # for
+      
+      Genre = item.get('Genre', None)
+      if Genre:
+         GenreName = ID3GenresClass.Genre(Genre)
+         if not GenreName:
+            logging.warning("Unknown genre for '%s': '%s'", InputFile, Genre)
+         cmd.extend(('--genre', GenreName, ))
+      # if
+      
+      iTrack = item.get('TrackNo', iElem + 1)
+      try:
+         iTrack, nTracks = map(int, iTrack.split('/', 1))
+      except (ValueError, AttributeError, ):
+         nTracks = item.get('NTracks', nElem)
+      cmd.extend(( '--track', "%s/%s" % (str(iTrack), str(nTracks)), ))
+      
+      for comment in item.get('Comments', []):
+         self.ValidateComment(comment)
+         cmd.append(( '--comment', comment, ))
+      # for
+      
+      cmd.append(InputFile)
+      return cmd
+      
+   # OutputCurrent()
+   
+   
+   def Output(self, OutputFile):
+      items = self.output
+      nItems = len(items)
+      Padding = len(str(nItems))
+      for iItem, item in enumerate(items):
+         
+         try:
+            InputFile = item['InputFile']
+         except KeyError:
+            raise RuntimeError("No file specified for item %d!", iItem)
+         cmd = (
+           'echo',
+           "[%0*d/%0*d] Processing '%s'" % (Padding, iItem+1, Padding, nItems, InputFile)
+         )
+         print >>OutputFile, " ".join(BashQuoterClass.QuoteList(cmd))
+         
+         cmd = self.OutputElement(item, iItem, nItems)
+         print >>OutputFile, " ".join(BashQuoterClass.QuoteList(cmd))
+      # for
+      cmd = [ 'echo', "Done." ]
+      
+      self.output = []
+      return nItems
+   # Output()
+   
 # class ProcessorClass
 
 
 
 def Process(InputFile, OutputFile, options):
-	"""Processes the input file"""
-	
-	Processor = ProcessorClass()
-	
-	Processor.Parse(InputFile)
-	
-	nCommands = Processor.Output(OutputFile)
-	
-	logging.info("%d comment types found: '%s'", len(Processor.commentKeys),
-	  "', '".join(Processor.commentKeys))
-	
+   """Processes the input file"""
+   
+   Processor = ProcessorClass()
+   
+   Processor.ParseFile(InputFile)
+   
+   nCommands = Processor.Output(OutputFile)
+   
+   logging.info("%d comment types found: '%s'", len(Processor.commentKeys),
+     "', '".join(Processor.commentKeys))
+   
 # def Process()
 
 
 
 if __name__ == "__main__":
-	
-	import argparse
-	
-	parser = argparse.ArgumentParser(
-          description="Creates a script assigning ID3 tags to MP3 files."
-          )
-	
-	parser.add_argument("InputFiles", nargs="*", action="store",
-	  help="input files [default: from stdin]")
-	
-	parser.add_argument("--output", action="store", dest="OutputFile",
-	  help="output file [default: stdout]", default=None)
-	parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-	
-	arguments = parser.parse_args()
-	
-	if not arguments.InputFiles: arguments.InputFiles = [ "" ]
-	
-	if arguments.OutputFile is None: OutputFile = sys.stdout
-	else:                            OutputFile = open(arguments.OutputFile, 'a')
-	
-	for InputPath in arguments.InputFiles:
-	  if not InputPath: InputFile = sys.stdin
-	  else:             InputFile = open(InputPath, 'r')
-	  
-	  Process(InputFile, OutputFile, arguments)
-	  
-	# for
-	
-	if OutputFile is not sys.stdout:
-		print "Output written in '%s'" % OutputFile.name
-		OutputFile.close()
-	# if
-	sys.exit(0)
+   
+   import argparse
+   
+   parser = argparse.ArgumentParser(
+      description=__doc__
+      )
+   
+   parser.add_argument("InputFiles", nargs="*", action="store",
+     help="input files [default: from stdin]")
+   
+   parser.add_argument("--output", action="store", dest="OutputFile",
+     help="output file [default: stdout]", default=None)
+   parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+   
+   arguments = parser.parse_args()
+   
+   if not arguments.InputFiles: arguments.InputFiles = [ "" ]
+   
+   if arguments.OutputFile is None: OutputFile = sys.stdout
+   else:                            OutputFile = open(arguments.OutputFile, 'a')
+   
+   for InputPath in arguments.InputFiles:
+     if not InputPath: InputFile = sys.stdin
+     else:             InputFile = open(InputPath, 'r')
+     
+     Process(InputFile, OutputFile, arguments)
+     
+   # for
+   
+   if OutputFile is not sys.stdout:
+      print "Output written in '%s'" % OutputFile.name
+      OutputFile.close()
+   # if
+   sys.exit(0)
 # main
