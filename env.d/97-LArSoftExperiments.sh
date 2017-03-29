a4ad5523d4fee49b6301fb9e6d3a0104b89b2908
Gianluca Petrillo <petrillo@fnal.gov>
2017-03-29 09:42:02 -0500

    Updated retrieval of FNAL certificate in grid_proxy

--- env.d/97-LArSoftExperiments.sh
+++ env.d/97-LArSoftExperiments.sh
@@ -55,7 +55,9 @@ function grid_proxy() {
 			echo "No grid certificate settings for experiment '${Experiment}'" >&2
 			return 1
 	esac
-	kx509
+	which cigetcert >& /dev/null || setup cigetcert
+	cigetcert -s 'fifebatch.fnal.gov' || return $?
+	# kx509
 	voms-proxy-init -noregen -rfc -voms "${Server}${Command:+":${Command}"}"
 } # grid_proxy()
 
