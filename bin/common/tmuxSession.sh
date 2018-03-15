6c14af6dd9d900e5a1e5e152c00e4d7a983fa610
Gianluca Petrillo <petrillo@fnal.gov>
2018-03-15 15:11:52 -0500

    Little changes to tmuxSession.sh and sshell

--- bin/common/tmuxSession.sh
+++ bin/common/tmuxSession.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 
 SessionCfgPath="$1"
+FallbackTMux="${HOME}/local/bin/tmux"
+
 : ${tmuxConfigDir:="${HOME}/etc/tmux"}
 
 SessionName="${2:-"$(basename "${SessionCfgPath%.conf}")"}"
 
-if ! tmux has-session -t "$SessionName" >& /dev/null ; then
+tmux="${FallbackTMux}"
+# tmux="$(which tmux 2> /dev/null)"
+# if [[ $? != 0 ]]; then
+# 	echo "Using fallback tmux at '${FallbackTMux}'"
+# 	tmux="${FallbackTMux}"
+# fi
+
+if [[ ! -x "$tmux" ]] ; then
+	echo "Can't run TMux ('${tmux}')." >&2
+	exit 2
+fi
+
+if ! $tmux has-session -t "$SessionName" >& /dev/null ; then
 	tmuxConfig="${tmuxConfigDir}/${SessionCfgPath}"
 	[[ -f "$tmuxConfig" ]] || tmuxConfig="${tmuxConfigDir}/${SessionCfgPath}.conf"
 	
 	if [[ ! -r "$tmuxConfig" ]] ; then
 		echo "tmux configuration file '${tmuxConfig}' not found, a plain session will be created."
-		tmux new-session -s "$SessionName"
-	elif tmux ls >& /dev/null ; then
+		$tmux new-session -s "$SessionName"
+	elif $tmux ls >& /dev/null ; then
 		echo "Creating a session with configuration file: '${tmuxConfig}'"
 
 		# although our session does not exist, the server is running
 		# (with other sessions); we don't need to start the server
-		tmux source-file "$tmuxConfig"
+		$tmux source-file "$tmuxConfig"
 	else
 		# creating a new server and a session using the configuration file;
 		# the configuration file is expected to create the session, detached
 		echo "Starting a server and creating a session with configuration file: '${tmuxConfig}'"
-		tmux -f "$tmuxConfig" start-server
+		$tmux -f "$tmuxConfig" start-server
 	fi
+	echo "Running sessions:"
+	$tmux ls
+else
+	echo "Session '${SessionName}' is already ongoing."
 fi
 
-tmux attach-session -d -t "$SessionName"
+echo "Attaching to session '${SessionName}'"
+$tmux attach-session -d -t "$SessionName"
 
