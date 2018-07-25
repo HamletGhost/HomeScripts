6bf6eab8bf92f2ca4ea5968c1cc189db99dc671b
Gianluca Petrillo <petrillo@slac.stanford.edu>
2018-07-25 16:56:24 -0500

    FollowLog.sh: follow symbolic links.

--- bin/common/FollowLog.sh
+++ bin/common/FollowLog.sh
@@ -37,7 +37,7 @@ else
 		CandidateLogDir="${LogDir}${SubLogDir:+/${SubLogDir}}"
 		: ${CandidateLogDir:="."}
 		[[ -d "$CandidateLogDir" ]] || continue
-		LogFile="$(find "$CandidateLogDir" ${Depth:+-maxdepth "$Depth"} -type f -name "$LOGPATTERN" | xargs ls -drt 2> /dev/null | tail -n 1)"
+		LogFile="$(find -L "$CandidateLogDir" ${Depth:+-maxdepth "$Depth"} -type f -name "$LOGPATTERN" | xargs ls -drt 2> /dev/null | tail -n 1)"
 		[[ "$LogFile" == '.' ]] && LogFile="" && continue # if find finds nothing, ls will show "."
 		[[ -f "$LogFile" ]] && break
 	done
