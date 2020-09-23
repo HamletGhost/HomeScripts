958e661ad9dc51e5dbe980e6beb2d34199ff8c5e
Gianluca Petrillo <petrillo@slac.stanford.edu>
2020-09-23 18:40:30 -0500

    cut_lines.py: improved implementation

--- bin/common/cut_lines.py
+++ bin/common/cut_lines.py
@@ -12,8 +12,10 @@ try: import gz
 except ImportError: pass
 try: import bz2
 except ImportError: pass
+import collections
 
 
+# ------------------------------------------------------------------------------
 def OPEN(filename, mode = 'r'):
   """Opens a file, read-only (possibly a compressed one)"""
   if filename.endswith('.gz'):
@@ -29,7 +31,30 @@ def OPEN(filename, mode = 'r'):
 # OPEN()
 
 
+# ------------------------------------------------------------------------------
+def FileTail(
+ file_: "file to read from",
+ n: "number of lines to keep from the end",
+ ) -> "a buffer with at most n lines from the end of the file (not stripped)":
+  return list(collections.deque(file_, n))
+# FileTail()
 
+
+def CutFileLines(
+ file_: "file to read from",
+ skipLines: "number of lines to skip" = 0,
+ n: "number of lines to keep (None to keep all)" = None,
+ ) -> "a buffer with at most n lines from startLine (not stripped)":
+  
+  for i in range(skipLines): file_.readline()
+  
+  if n is None: return list(file_)
+  else: return list(file_.readline() for i in range(n))
+  
+# CutFileLines()
+
+
+# ------------------------------------------------------------------------------
 if __name__ == '__main__':
   
   import argparse
@@ -46,9 +71,11 @@ if __name__ == '__main__':
   Parser.add_argument("--startline", "-s", type=int, default=0,
     help="the number of starting line; negative means starting from the end"
     " (the last line is -1) [%(default)d]")
-  Parser.add_argument("--lines", "-n", type=int, default=1,
-    help="number of lines to be printed (if available); negative means all"
-      " left ones [%(default)d]")
+  Parser.add_argument("--stopline", "-S", type=int,
+    help="the number of first line not printed; overrides `--lines` option")
+  Parser.add_argument("--lines", "-n", type=int,
+    help="number of lines to be printed (if available); negative goes backward"
+      " (default: print all)")
   Parser.add_argument("--stdin", "-c", action="store_true", dest="use_stdin",
     help="reads from standard input AFTER reading all input files")
   Parser.add_argument("--nouncompress", "-C", action="store_true",
@@ -62,14 +89,15 @@ if __name__ == '__main__':
   
   args = Parser.parse_args()
 
-  # check option values
-#  if args.lines <= 0:
-#     Parser.error(
-#      "--lines options requires a positive number of lines, not '%d'"
-#      % args.lines)
-  
+  startline = args.startline
+  lines = args.lines
   if args.stopline is not None:
-    args.lines = max(args.stopline - args.startline, 0)
+    lines = max(args.stopline - startline, 0)
+  
+  if lines is not None and lines < 0:
+    startline += lines
+    lines = -lines
+  #
   
   InputFiles = args.InputFiles[:]
 
@@ -87,27 +115,11 @@ if __name__ == '__main__':
         print("Can't open source file '%s'." % FileName, file=sys.stderr)
         raise
     
-    Buffer = []
     # go for it!
-    if args.startline < 0: # start keeping lines
-      BufLen = -args.startline
-      for line in File:
-        Buffer.append(line)
-        while len(Buffer) > BufLen: del Buffer[0]
-      # for
-      # we don't really need the following lines:
-      if args.lines >= 0: del Buffer[args.lines:]
+    if startline < 0: # start keeping lines
+      Buffer = FileTail(File, n=-startline)[:lines]
     else: # start skipping lines
-      BufLen = args.lines
-      if args.startline > 1:
-        nLeft = args.startline - 1
-        for line in File:
-          nLeft -= 1
-          if nLeft <= 0: break
-      for line in File:
-        if BufLen >= 0 and len(Buffer) >= BufLen: break
-        Buffer.append(line)
-      # for
+      Buffer = CutFileLines(File, skipLines=(startline - 1), n=lines)
     # if
     
     if args.verbose:
