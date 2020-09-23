42960177ab31aa535723c4e457332fd679ca5d4d
Gianluca Petrillo <petrillo@slac.stanford.edu>
2020-09-23 17:31:06 -0500

    cut_lines.py: version bump and Python 3

--- bin/common/cut_lines.py
+++ bin/common/cut_lines.py
@@ -1,100 +1,129 @@
 #!/usr/bin/env python
 
-import sys
-import optparse
-
-Version = "%prog 0.1"
-UsageMsg = """
+__version__ = "1.0"
+__doc__ = """
 Prints the specified lines of each file.
-
-Usage:  %prog [options] [InputFile] [InputFile] ...
+If a file is compressed by gzip or bzip2, it is uncompressed.
 """
+
+import sys
+
+try: import gz
+except ImportError: pass
+try: import bz2
+except ImportError: pass
+
+
+def OPEN(filename, mode = 'r'):
+  """Opens a file, read-only (possibly a compressed one)"""
+  if filename.endswith('.gz'):
+    OpenProc = gzip.open
+  elif filename.endswith('.bz2'):
+    OpenProc = bz2.BZ2File.__call__
+  else:
+    OpenProc = open
+  f = OpenProc(filename, mode)
+  # GzipFile from Python 2.4 has no name attribute (it has "filename" though)
+  if not hasattr(f, 'name'): f.name = filename
+  return f
+# OPEN()
+
 
 
 if __name__ == '__main__':
-	
-	Parser = optparse.OptionParser(usage=UsageMsg,
-		version=Version, add_help_option=False)
-	
-	Parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
-		default=False, help="prints a header line for each input file")
-#	Parser.add_option("-q", "--quiet", action="store_false", dest="verbose", 
-#		help="doesn't print anything on the screen (useful, huh?)")
-	Parser.add_option("-s", "--startline", dest="startline", type="int",
-		default=0,
-		help="the number of starting line; negative means starting from the end"
-		" (the last line is -1) [%default]")
-	Parser.add_option("-n", "--lines", dest="lines", type="int",
-		default=1,
-		help="number of lines to be printed (if available); negative means all"
-			" left ones [%default]")
-	Parser.add_option("-c", "--stdin", action="store_true", dest="use_stdin",
-		help="reads fron standard input AFTER reading all input files")
-	Parser.add_option("-h", "--help", dest="doHelp",
-		action="store_true", default=False, help="prints this help message")
-	
-	(options, FileNames) = Parser.parse_args()
-	
-	if options.doHelp:
-		Parser.print_help()
-		sys.exit(0)
-	
-	# check option values
-#	if options.lines <= 0:
-#		Parser.error(
-#			"--lines options requires a positive number of lines, not '%d'"
-#			% options.lines)
-		
-	
-	if len(FileNames) == 0: options.use_stdin = True
-	elif options.use_stdin is None: options.use_stdin = False
-	
-	if options.use_stdin: FileNames.append(None)
-	
-	for FileName in FileNames:
-		if FileName is None: file = sys.stdin
-		else:
-			try:
-				file = open(FileName, "r")
-			except IOError:
-				print >>sys.stderr, "Can't open source file '%s'." % FileName
-				raise
-		
-		Buffer = []
-		# go for it!
-		if options.startline < 0: # start keeping lines
-			BufLen = -options.startline
-			for line in file:
-				Buffer.append(line)
-				while len(Buffer) > BufLen: del Buffer[0]
-			# for
-			# we don't really need the following lines:
-			if options.lines >= 0: del Buffer[options.lines:]
-		else: # start skipping lines
-			BufLen = options.lines
-			if options.startline > 1:
-				nLeft = options.startline - 1
-				for line in file:
-					nLeft -= 1
-					if nLeft <= 0: break
-			for line in file:
-				if BufLen >= 0 and len(Buffer) >= BufLen: break
-				Buffer.append(line)
-			# for
-		# if
-		
-		if options.verbose:
-			print >>sys.stderr, 80*"-"
-			if FileName is None: print >>sys.stderr, "Standard input:"
-			else: print >>sys.stderr, "File: '%s'" % FileName
-		
-		# note that lines have not been stripped!
-		for line in Buffer: print line,
-		
-		# close input file - if not stdin!
-		if FileName is not None: file.close()
-	# for FileName
-	
-	
-	sys.exit(0)
+  
+  import argparse
+  
+  Parser = argparse.ArgumentParser(description=__doc__)
+  
+  Parser.add_argument("InputFiles", nargs='*',
+    help="files to extract lines from")
+  
+  Parser.add_argument("--verbose", "-v", action="store_true",
+    help="prints a header line for each input file")
+#  Parser.add_argument("--quiet", "-q", action="store_false", dest="verbose", 
+#   help="doesn't print anything on the screen (useful, huh?)")
+  Parser.add_argument("--startline", "-s", type=int, default=0,
+    help="the number of starting line; negative means starting from the end"
+    " (the last line is -1) [%(default)d]")
+  Parser.add_argument("--lines", "-n", type=int, default=1,
+    help="number of lines to be printed (if available); negative means all"
+      " left ones [%(default)d]")
+  Parser.add_argument("--stdin", "-c", action="store_true", dest="use_stdin",
+    help="reads from standard input AFTER reading all input files")
+  Parser.add_argument("--nouncompress", "-C", action="store_true",
+    dest="DontUncompress",
+    help="don't uncompress gzip and bzip2 files [%(default)s]"
+    )
+  
+  Parser.add_argument('--version', action="version",
+    version="%(prog)s version {}".format(__version__)
+    )
+  
+  args = Parser.parse_args()
+
+  # check option values
+#  if args.lines <= 0:
+#     Parser.error(
+#      "--lines options requires a positive number of lines, not '%d'"
+#      % args.lines)
+  
+  if args.stopline is not None:
+    args.lines = max(args.stopline - args.startline, 0)
+  
+  InputFiles = args.InputFiles[:]
+
+  if len(InputFiles) == 0: args.use_stdin = True
+  elif args.use_stdin is None: args.use_stdin = False
+  
+  if args.use_stdin: InputFiles.append(None)
+  
+  for FileName in InputFiles:
+    if FileName is None: File = sys.stdin
+    else:
+      try:
+        File = (open if args.DontUncompress else OPEN)(FileName, mode='r')
+      except IOError:
+        print("Can't open source file '%s'." % FileName, file=sys.stderr)
+        raise
+    
+    Buffer = []
+    # go for it!
+    if args.startline < 0: # start keeping lines
+      BufLen = -args.startline
+      for line in File:
+        Buffer.append(line)
+        while len(Buffer) > BufLen: del Buffer[0]
+      # for
+      # we don't really need the following lines:
+      if args.lines >= 0: del Buffer[args.lines:]
+    else: # start skipping lines
+      BufLen = args.lines
+      if args.startline > 1:
+        nLeft = args.startline - 1
+        for line in File:
+          nLeft -= 1
+          if nLeft <= 0: break
+      for line in File:
+        if BufLen >= 0 and len(Buffer) >= BufLen: break
+        Buffer.append(line)
+      # for
+    # if
+    
+    if args.verbose:
+      print(80*"-", file=sys.stderr)
+      print(
+        "Standard input:" if FileName is None else ("File: '%s'" % FileName),
+        file=sys.stderr, flush=True
+        )
+    
+    # note that lines have not been stripped!
+    for line in Buffer: print(line, end='')
+    
+    # close input file - if not stdin!
+    if FileName is not None: File.close()
+  # for FileName
+  
+  
+  sys.exit(0)
 # main
