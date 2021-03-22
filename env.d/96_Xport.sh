3dba9a05e28768b72c8bdce4d12a8769353c01ff
Gianluca Petrillo <petrillo@slac.stanford.edu>
2021-03-22 11:55:22 -0500

    Xport command supports some DISPLAY autodetection.

--- env.d/96_Xport.sh
+++ env.d/96_Xport.sh
@@ -1,20 +1,65 @@
 #!/usr/bin/env bash
 
 function Xport() {
-  declare Port="$1"
+  # if Port is specified ( [host:]port[.screen] ), use it;
+  # otherwise attempt autodetection;
+  # always be verbose...
+  local Port="$1"
   
-  # TODO: find a way to autodetect the right port
-  if [[ -z "$Port" ]]; then
-    cat <<EOH
-Please specify which port to set for DISPLAY.
-EOH
+  local -r Autodetect=1 # maybe one day we'll want this optional
+  
+  local RecommendedHost
+  local RecommendedPort
+  local RecommendedScreen
+  local RecommendedDisplay
+
+  if [[ "$Autodetect" != 0 ]]; then
+ 
+    # find a way to autodetect the right port
+    if [[ -n "$TMUX" ]]; then
+      # we are in tmux environment: let's ask tmux
+      DisplayLine="$(tmux show-env | grep -E '^DISPLAY=')"
+    
+      if [[ "$DisplayLine" =~ DISPLAY=(.*):([0-9]+)\.([0-9]+) ]]; then
+        RecommendedHost="${BASH_REMATCH[1]}"
+        RecommendedPort="${BASH_REMATCH[2]}"
+        RecommendedScreen="${BASH_REMATCH[3]}"
+        RecommendedDisplay="${RecommendedHost}:${RecommendedPort}.${RecommendedScreen}"
+      fi
+    else
+      : # no other smart way...
+    fi
+  fi # if autodetection
+
+  if [[ -n "$Port" ]]; then
+    [[ "${Port//.}" == "$Port" ]] && Port+=".0"
+    [[ "${Port//:}" == "$Port" ]] && Port="localhost:${Port}"
+  fi
+
+  local Display="${Port:-${RecommendedDisplay}}"
+  if [[ -z "$Display" ]]; then
+    echo "ERROR: no port specified, and auto-detection failed." >&2
     return 1
   fi
-  
-  [[ "${Port//.}" == "$Port" ]] && Port+=".0"
-  
-  export DISPLAY="localhost:${Port}"
-  
+
+  if [[ "$DISPLAY" == "$Display" ]]; then
+    echo "DISPLAY already set to ${Display}."
+  else
+    
+    if [[ -z "$DISPLAY" ]]; then
+      echo "Display was not set."
+    elif [[ "$DISPLAY" =~ localhost:([0-9]+)\.([0-9]+) ]]; then
+      echo "Display was set on port ${BASH_REMATCH[1]}."
+    else
+      echo "Display was set as '${DISPLAY}'."
+    fi
+ 
+    export DISPLAY="$Display"
+    echo "DISPLAY now set to '${DISPLAY}'."
+  fi
+  if [[ -n "$RecommendedDisplay" ]] && [[ "$RecommendedDisplay" != "$Display" ]]; then
+    echo "Warning: DISPLAY recommended setting is '${RecommendedDisplay}'."
+  fi
   return 0
 } # Xport()
 
