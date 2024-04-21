9ac4313f032c3739bad9d6a929b5e0f02310d139
Hamlet (as administrator) <hamletghost@gmail.com>
2024-04-20 22:46:54 -0700

    Updates for Gentoo system

--- bin/sysadmin/es.sh
+++ bin/sysadmin/es.sh
@@ -4,9 +4,6 @@
 : ${tmuxConfigDir:="${HOME}/bin/sysadmin/params/tmux"}
 
 cd
-mount /usr/portage
-umount ~ftp/repository/gentoo-portage
-mount ~ftp/repository/gentoo-portage
 
 export tmuxConfigDir
 tmuxSession.sh "$SessionName"
