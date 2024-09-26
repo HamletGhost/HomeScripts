d74ee1eaa93cf7cba8b4fa71072e26b72f21eaeb
Hamlet <hamletghost@gmail.com>
2024-09-26 11:15:40 -0700

    CloseKDE.sh: compatible with Plasma6

--- bin/common/CloseKDE.sh
+++ bin/common/CloseKDE.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+if which qdbus6 >& /dev/null ; then
+    KDEplasma=6
+else
+    KDEPlasma=5
+fi
+
+case "$KDEPlasma" in
+  ( 5 ) qdbus org.kde.ksmserver /KSMServer logout 0 0 0 ;;
+  ( 6 ) qdbus6 org.kde.Shutdown /Shutdown org.kde.Shutdown.logout ;;
+  ( * ) echo "${BASH_SOURCE}: internal error (KDEPlasma='${KDEPlasma}')." ;;
+esac
+
